ca655c186665a1cd1ed27585df9ce76806254a37
irisHanb <fortrulymylife@gmail>
2019-09-13 17:31:38 +0900

    Updates

---
+++ precache-manifest.b3127a60b298d7d93e6120555474248a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1797f65028793cea8e2d4497ae03f8",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e994c078669f7c9f6b10",
+    "url": "/movie_app_2019/static/css/main.47e52523.chunk.css"
+  },
+  {
+    "revision": "ee64cf36bfb72cf75153",
+    "url": "/movie_app_2019/static/js/2.d1bb7e12.chunk.js"
+  },
+  {
+    "revision": "e994c078669f7c9f6b10",
+    "url": "/movie_app_2019/static/js/main.fa0faf5a.chunk.js"
+  },
+  {
+    "revision": "29aaad419714eb018816",
+    "url": "/movie_app_2019/static/js/runtime~main.f4904fd8.js"
+  }
+]);
